bafa5d422897e25bf9634bd5446d3d52ed80de0c
Gabriel Piro <pirogabriel5@gmail.com>
2026-05-18 12:07:29 -0300

    Add backend-ci

---
+++ src/test/java/com/example/demo/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+}
